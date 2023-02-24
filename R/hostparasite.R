4dd6c4d377457b5753baff7964a7c615a366bb81
Saint-Clair Chabert-Liddell <saint-clair.chabert-liddell@inrae.fr>
2023-02-24 16:56:59 +0100

    For cran release

--- R/hostparasite.R
+++ R/hostparasite.R
@@ -3,6 +3,6 @@
 #' Locality: Tomsk-Tumen
 #' Reference:Hadfield JD, Krasnov BR, Poulin R, Shinichi N (2013) A tale of two phylogenies: comparative analyses of ecological interactions. The American Naturalist 183(2): 174-187
 #' @format A matrix of binary entries with 27 rows and 26 columns
-#' @source \url{http://www.web-of-life.es/}
+#' @source \url{https://www.web-of-life.es/}
 #' @source \url{https://datadryad.org/stash/dataset/doi:10.5061/dryad.jf3tj}
 "hostparasite"
--- R/seeddispersal.R
+++ R/seeddispersal.R
@@ -3,6 +3,6 @@
 #' Locality: North Negros Forest Reserve, Central Philippi
 #' Hammann, A. & Curio, B. (1999) Interactions among frugivores and fleshy fruit trees in a Philippine submontane rainforest.
 #' @format A matrix of binary entries with 36 rows and 19 columns
-#' @source \url{http://www.web-of-life.es/}
+#' @source \url{https://www.web-of-life.es/}
 #' @source \url{https://conbio.org/publications/conservation-biology}
 "seeddispersal"
